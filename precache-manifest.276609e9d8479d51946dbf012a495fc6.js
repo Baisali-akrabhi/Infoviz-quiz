c2f7a6606fec61872f075cd482d775e07b1a9c6b
baisali pradhan <baisali.pradhan@infoviz.co>
2020-08-28 17:16:40 +0530

    Updates

---
+++ precache-manifest.276609e9d8479d51946dbf012a495fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe3fae971c6fa9d87eee6966bc524d2",
+    "url": "/Infoviz-quiz/index.html"
+  },
+  {
+    "revision": "48d20baa728d8d88b357",
+    "url": "/Infoviz-quiz/static/css/main.6a2a2929.chunk.css"
+  },
+  {
+    "revision": "a0868884e713bdaa1788",
+    "url": "/Infoviz-quiz/static/js/2.1594debc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Infoviz-quiz/static/js/2.1594debc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d20baa728d8d88b357",
+    "url": "/Infoviz-quiz/static/js/main.bd87b9fd.chunk.js"
+  },
+  {
+    "revision": "c7c222cd4030cdf68811",
+    "url": "/Infoviz-quiz/static/js/runtime-main.3e628832.js"
+  }
+]);
